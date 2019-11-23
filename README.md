5222380dba6e299af6968484da124071942104f7
David Carlisle <d.p.carlisle@gmail.com>
2019-11-23 09:37:17 +0000

    update for new location

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # attachfile2
 attach files to PDF documents
+
+
+
+See the embedfile package for a newer package supporting this feature.
+
+
+This package comes with a Perl script pdfatfi.pl
+that should be installed somewhere in PATH as `pdfatfi', see also
+package documentation.
---
+++ build.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/env texlua
+
+
+module = "attachfile2"
+
+textfiles = {"README.md"}
+unpackfiles = {'attachfile2.dtx'}
+sourcefiles={"*.dtx"}
+scriptfiles={"*.pl"}
+installfiles={"*.sty","*.def","*.pl"}
+
+packtdszip  = true
+
+packtdszip  = true
+
+maxprintline=10000
+checkruns = 2
+
+
+
+
+
