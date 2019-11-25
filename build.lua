c6c868bba9b8afbcc3798ca5a239587a9347a850
David Carlisle <d.p.carlisle@gmail.com>
2019-11-25 23:25:35 +0000

    add man file as requested by texlive

--- build.lua
+++ build.lua
@@ -7,13 +7,16 @@ textfiles = {"README.md"}
 unpackfiles = {'attachfile2.dtx'}
 sourcefiles={"*.dtx"}
 scriptfiles={"*.pl"}
-installfiles={"*.sty","*.def","*.pl"}
+installfiles={"*.sty","*.def","*.pl","*.1"}
 
+tdslocations={
+"doc/man/man1/pdfatfi.1"
+}
 testfiledir= "disabled" -- issues with reproducible attachments
 
 packtdszip  = true
 
-packtdszip  = true
+	    packtdszip  = true
 
 maxprintline=10000
 checkruns = 2
