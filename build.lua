3376c7fb4affe8155bb9dd61493baa6155819c2d
David Carlisle <d.p.carlisle@gmail.com>
2020-02-26 20:36:46 +0000

    update auto tag to match luacolor

--- build.lua
+++ build.lua
@@ -21,7 +21,36 @@ packtdszip  = true
 maxprintline=10000
 checkruns = 2
 
+function update_tag(file,content,tagname,tagdate)
 
+local tagpattern="(%d%d%d%d[-/]%d%d[-/]%d%d) v(%d+[.])(%d+)"
+local oldv,newv
+if tagname == 'auto' then
+  local i,j,olddate,a,b
+  i,j,olddate,a,b= string.find(content, tagpattern)
+  if i == nil then
+    print('OLD TAG NOT FOUND')
+    return content
+  else
+    print ('FOUND: ' .. olddate .. ' v' .. a .. b )
+    oldv = olddate .. ' v' .. a .. b
+    newv = tagdate .. ' v'  .. a .. math.floor(b + 1)
+    print('USING OLD TAG: ' .. oldv)
+    print('USING NEW TAG: ' .. newv)
+    local oldpattern = string.gsub(oldv,"[-/]", "[-/]")
+    content=string.gsub(content,"{Version}{" .. oldpattern,'##OLDV##')
+    content=string.gsub(content,oldpattern,newv)
+    content=string.gsub(content,'##OLDV##',"{Version}{" .. oldv)
+    content=string.gsub(content,'%-%d%d%d%d Oberdiek Package','-' .. os.date("%Y") .. " Oberdiek Package")
+    content = string.gsub(content,
+        '%% \\end{History}',
+	'%%   \\begin{Version}{' .. newv .. '}\n%%   \\item Updated\n%%   \\end{Version}\n%% \\end{History}')
+    return content
+  end
+else
+  error("only automatic tagging supported")
+end
 
+end
 
 
